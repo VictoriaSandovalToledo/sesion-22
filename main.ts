470e1eaee021c2300f042016b1145c8a070733db
VictoriaSandovalToledo <110991718+VictoriaSandovalToledo@users.noreply.github.com>
2022-10-12 07:46:54 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+let sprite1 = game.createSprite(0, randint(0, 10))
+let sprite2 = game.createSprite(randint(1, 4), randint(0, 10))
+basic.forever(function () {
+    while (input.buttonIsPressed(Button.AB)) {
+        sprite1.change(LedSpriteProperty.X, 1)
+        basic.pause(200)
+    }
+    while (input.logoIsPressed()) {
+        sprite1.change(LedSpriteProperty.X, -1)
+        basic.pause(200)
+    }
+    while (input.buttonIsPressed(Button.A)) {
+        sprite1.change(LedSpriteProperty.Y, 1)
+        basic.pause(200)
+    }
+    while (input.buttonIsPressed(Button.B)) {
+        sprite1.change(LedSpriteProperty.Y, -1)
+        basic.pause(100)
+    }
+})
